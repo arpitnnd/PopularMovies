65a7f73c554ca4b420779cb6055225e984690eb5
Arpit Anand <arpit_nnd@outlook.com>
2016-02-18 04:05:12 +0530

    * Implemented API calls.

--- app/src/main/java/com/arpitnnd/popularmovies/MoviesFragment.java
+++ app/src/main/java/com/arpitnnd/popularmovies/MoviesFragment.java
@@ -13,6 +13,16 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -20,7 +30,8 @@ public class MoviesFragment extends Fragment {
 
     static GridView gridView;
     static ArrayList<String> posters;
-    static boolean sortByPop;
+    static boolean sortByPopularity;
+    static String API_KEY = "c7471dc1c8bc98ab58f7596dbb9a7888";
 
     public MoviesFragment() {
     }
@@ -52,7 +63,6 @@ public class MoviesFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        getActivity().setTitle("Most Popular Movies");
 
         if (isNetworkAvailable()) {
             gridView.setVisibility(GridView.VISIBLE);
@@ -74,7 +84,7 @@ public class MoviesFragment extends Fragment {
         protected ArrayList<String> doInBackground(Void... params) {
             while (true) {
                 try {
-                    posters = new ArrayList(Arrays.asList(getPathsFromAPI(sortByPop)));
+                    posters = new ArrayList(Arrays.asList(getAPIPaths(sortByPopularity)));
                     return posters;
                 } catch (Exception e) {
                     continue;
@@ -92,14 +102,80 @@ public class MoviesFragment extends Fragment {
             }
         }
 
-        public String[] getPathsFromAPI(boolean sort) {
-            String[] array = new String[15];
-            for (int i = 0; i < array.length; i++) {
-                array[i] = "/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg";
-            }
-            return array;
+        public String[] getAPIPaths(boolean sortby) {
+            {
+                while (true) {
+                    HttpURLConnection urlConnection = null;
+                    BufferedReader reader = null;
+                    String JSONResult;
 
+                    try {
+                        String urlString = null;
+                        if (sortby) {
+                            urlString = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + API_KEY;
+                        } else {
+                            urlString = "http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&vote_count.gte=500&api_key=" + API_KEY;
+                        }
+                        URL url = new URL(urlString);
+                        urlConnection = (HttpURLConnection) url.openConnection();
+                        urlConnection.setRequestMethod("GET");
+                        urlConnection.connect();
+
+                        //Read the input stream into a String
+                        InputStream inputStream = urlConnection.getInputStream();
+                        StringBuffer buffer = new StringBuffer();
+                        if (inputStream == null) {
+                            return null;
+                        }
+                        reader = new BufferedReader(new InputStreamReader(inputStream));
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            buffer.append(line + "\n");
+                        }
+                        if (buffer.length() == 0) {
+                            return null;
+                        }
+                        JSONResult = buffer.toString();
+
+                        try {
+                            return getPathsFromJSON(JSONResult);
+                        } catch (JSONException e) {
+                            return null;
+                        }
+                    } catch (Exception e) {
+                        continue;
+                    } finally {
+                        if (urlConnection != null) {
+                            urlConnection.disconnect();
+                        }
+                        if (reader != null) {
+                            try {
+                                reader.close();
+                            } catch (final IOException e) {
+                            }
+                        }
+                    }
+
+
+                }
+            }
         }
+
+        public String[] getPathsFromJSON(String JSONStringParam) throws JSONException {
+
+            JSONObject JSONString = new JSONObject(JSONStringParam);
+
+            JSONArray moviesArray = JSONString.getJSONArray("results");
+            String[] result = new String[moviesArray.length()];
+
+            for (int i = 0; i < moviesArray.length(); i++) {
+                JSONObject movie = moviesArray.getJSONObject(i);
+                String moviePath = movie.getString("poster_path");
+                result[i] = moviePath;
+            }
+            return result;
+        }
+
     }
 
 }
